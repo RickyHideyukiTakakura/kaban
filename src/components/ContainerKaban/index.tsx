8016470d56b41e1b6b6a1f3b95409c3e4b15a851
Ricky Hideyuki Takakura <rickytakakura@outlook.com>
2023-04-30 22:33:42 -0300

    fix: corrections last commit

--- src/components/ContainerKaban/index.tsx
+++ src/components/ContainerKaban/index.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import { Card } from '../Card';
 import { Kaban, KabanBody, KabanHeader } from '../Kaban';
 import * as S from './styles';
--- src/main.tsx
+++ src/main.tsx
@@ -1,30 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { App } from './App'
-import { Model, createServer } from 'miragejs'
-
-createServer({
-  models: {
-    card: Model
-  },
-
-  routes() {
-    this.namespace = '/api'
-
-    this.get('/cards', () => {
-      return this.schema.all('transaction')
-    })
-
-    this.post('/cards', (schema, request) => {
-      const data = JSON.parse(request.requestBody)
-
-      return schema.create('card', data)
-    })
-  }
-})
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { App } from './App';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
